f2ca0d9bd281b4a7995d05be8855ad7532835ff2
Anton <anton@adc.sh>
2018-06-14 13:44:09 +0100

    test

--- test/context/index.js
+++ test/context/index.js
@@ -1,9 +1,26 @@
+import { resolve } from 'path'
+
 /**
  * A testing context for the package.
  */
 export default class Context {
   async _init() {
-    console.log('init context')
+    // console.log('init context')
+  }
+  get SNAPSHOT_DIR() {
+    return resolve(__dirname, '../snapshot')
+  }
+  get DATA() {
+    return [
+      {
+        name: 'hello',
+        when: 'yesterday',
+      },
+      {
+        name: 'world',
+        when: 'now',
+      },
+    ]
   }
   /**
    * Example method.
@@ -12,6 +29,6 @@ export default class Context {
     return 'OK'
   }
   async _destroy() {
-    console.log('destroy context')
+    // console.log('destroy context')
   }
 }
--- test/spec/index.js
+++ test/spec/index.js
@@ -1,18 +1,62 @@
 import { equal } from 'zoroaster/assert'
+import SnapshotContext from 'snapshot-context'
 import Context from '../context'
 import tablature from '../../src'
 
-/** @type {Object.<string, (c: Context)>} */
+/** @type {Object.<string, (c: Context, s: SnapshotContext)>} */
 const T = {
-  context: Context,
+  context: [
+    Context,
+    SnapshotContext,
+  ],
   'is a function'() {
     equal(typeof tablature, 'function')
   },
-  async 'calls package without error'() {
-    await tablature()
+  async 'prints correct data'({ DATA, SNAPSHOT_DIR }, { setDir, test }) {
+    setDir(SNAPSHOT_DIR)
+    const res = tablature({
+      keys: ['name', 'when'],
+      data: DATA,
+    })
+    await test('normal.txt', res.trim())
   },
-  async 'calls test context method'({ example }) {
-    await example()
+  async 'centers heading'({ DATA, SNAPSHOT_DIR }, { setDir, test }) {
+    setDir(SNAPSHOT_DIR)
+    const res = tablature({
+      keys: ['name', 'when'],
+      data: DATA,
+      centerHeadings: ['when'],
+    })
+    await test('heading-center.txt', res.trim())
+  },
+  async 'centers values'({ DATA, SNAPSHOT_DIR }, { setDir, test }) {
+    setDir(SNAPSHOT_DIR)
+    const res = tablature({
+      keys: ['name', 'when'],
+      data: DATA,
+      centerValues: ['when'],
+      headings: {
+        when: 'When',
+      },
+    })
+    await test('values-center.txt', res.trim())
+  },
+  async 'replaces values'({ DATA, SNAPSHOT_DIR }, { setDir, test }) {
+    setDir(SNAPSHOT_DIR)
+    const res = tablature({
+      keys: ['name', 'when'],
+      data: DATA,
+      replacements: {
+        when(v) {
+          const value = `--${v}--`
+          return {
+            value,
+            length: value.length,
+          }
+        },
+      },
+    })
+    await test('replacements.txt', res.trim())
   },
 }
 
